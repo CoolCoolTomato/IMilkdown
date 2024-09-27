20a7138956a5c93d98cdc80dab1b1d7e91a718bf
CoolCoolTomato <2983315455@qq.com>
2024-09-27 15:12:03 +0800

    封装文件操作

---
+++ Server/sdk/sdk.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+
+def read_file(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def write_file(path, content):
+    with open(path, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+def get_files(path):
+    return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+
+def get_folders(path):
+    return [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
+
+def create_file(path):
+    with open(path, 'w', encoding='utf-8') as file:
+        pass
+
+def create_folder(path):
+    os.makedirs(path, exist_ok=True)
+
+def rename_file(old_path, new_path):
+    os.rename(old_path, new_path)
+
+def rename_folder(old_path, new_path):
+    os.rename(old_path, new_path)
+
+def delete_file(path):
+    os.remove(path)
+
+def delete_folder(path):
+    shutil.rmtree(path)
