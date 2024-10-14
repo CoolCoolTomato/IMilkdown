d69a11bc9516cf0ad1d27eb1a5fef3f741efe9df
CoolCoolTomato <2983315455@qq.com>
2024-10-14 19:05:26 +0800

    引入milkdown

---
+++ Web/src/components/Milkdown.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="milkdown"/>
+</template>
+<script>
+import { Crepe } from '@milkdown/crepe'
+import { replaceAll, getMarkdown } from '@milkdown/kit/utils'
+
+import "@milkdown/crepe/theme/common/style.css"
+import "@milkdown/crepe/theme/frame.css"
+
+export default {
+  data() {
+    return {
+      editor: null
+    }
+  },
+  methods: {
+    async init_milkdown() {
+      let crepe = new Crepe({
+        root: document.getElementById('milkdown'),
+        defaultValue: 'Hello, Milkdown!',
+      })
+
+      await crepe.create()
+      this.editor = crepe.editor
+    },
+    set_text(text) {
+      this.editor.action(replaceAll(text))
+    },
+    get_text() {
+      return this.editor.action(getMarkdown())
+    },
+  },
+  async mounted() {
+    await this.init_milkdown()
+  }
+}
+</script>
--- Web/src/views/MainWindow.vue
+++ Web/src/views/MainWindow.vue
@@ -1,16 +1,38 @@
 <template>
   <div id="main">
-    <tree-view :node="nodes" text="name" children="sub_nodes" show-children="is_folder"/>
+    <div style="display: flex; height: 100%">
+      <splitpanes class="default-theme">
+        <pane max-size="20">
+          <tree-view :node="nodes" text="name" children="sub_nodes" show-children="is_folder"/>
+        </pane>
+        <pane>
+          <div id="editor">
+            <div id="text">
+              <textarea/>
+            </div>
+            <div id="markdown">
+              <Milkdown />
+            </div>
+          </div>
+        </pane>
+      </splitpanes>
+    </div>
   </div>
 </template>
 
 <script>
 import nodeApi from "@/api/nodeApi.js"
 import TreeView from "@/components/TreeView.vue"
+import Milkdown from "@/components/Milkdown.vue"
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
 
 export default {
   components: {
-    TreeView
+    TreeView,
+    Milkdown,
+    Splitpanes,
+    Pane
   },
   data() {
     return {
@@ -40,5 +62,28 @@ export default {
   height: 100vh;
   background: #EEEEEE;
 }
-
+#editor {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+#text {
+  overflow-y: scroll;
+}
+#text textarea {
+  width: 100%;
+  height: 100%;
+  resize: none;
+  border: none;
+  padding: 0;
+  box-shadow: none;
+}
+#text textarea:focus {
+  border: none;
+  box-shadow: none;
+}
+#markdown {
+  overflow-y: scroll;
+}
 </style>
